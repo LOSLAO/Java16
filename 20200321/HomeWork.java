056e14caa42c44f4789a1690922550dbb938c8d3
loslao <1197758543@qq.com>
2020-04-07 21:33:13 +0800

    类和对象

---
+++ 20200321/HomeWork.java
@@ -0,0 +1,21 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: GAOBO
+ * Date: 2020-03-21
+ * Time: 8:52
+ */
+
+class Test {
+    public static void hello() {
+        System.out.println("hello");
+    }
+}
+public class HomeWork {
+    public static void main(String[] args) {
+        Test test=null;
+        test.hello();
+        /*int[] array = null;
+        System.out.println(array.length);*/
+    }
+}
