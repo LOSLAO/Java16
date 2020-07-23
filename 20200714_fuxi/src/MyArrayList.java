246ca4642afae885b12c41dfd46d2bdbf4fd699e
loslao <1197758543@qq.com>
2020-07-23 20:57:11 +0800

    单链表复习

---
+++ 20200714_fuxi/src/MyArrayList.java
@@ -0,0 +1,19 @@
+
+
+
+public class MyArrayList {
+    private int[] elem;
+    private int usedSize;
+
+    private static final int DEFAULT_SIZE = 3;
+
+    public MyArrayList() {
+        this.elem = new int[DEFAULT_SIZE];
+        this.usedSize = usedSize;
+    }
+
+    //在pos位置新增元素
+    public void add(int pos, int data) {
+        if ()
+    }
+}
