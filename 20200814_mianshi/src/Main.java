22eea0da07fe43a39485a3cda9d5904b61e5bf59
loslao <1197758543@qq.com>
2020-08-20 20:11:03 +0800

    面试题

---
+++ 20200814_mianshi/src/Main.java
@@ -0,0 +1,33 @@
+
+
+
+
+public class Main {
+    //旋转数组的最小数字
+    public int minArray(int[] numbers) {
+        int i = 0;
+        int j = numbers.length - 1;
+        while (i < j) {
+            int m = (i + j) / 2;
+            if (numbers[m] > numbers[j]) {
+                i = m + 1;
+            } else if (numbers[m] < numbers[j]) {
+                j = m;
+            } else {
+                j--;
+            }
+        }
+        return numbers[i];
+    }
+
+    //二进制中1的个数
+    public int hammingWeight(int n) {
+        int count = 0;
+        while (n != 0) {
+            count++;
+            n = n & (n - 1);
+        }
+        return count;
+
+    }
+}
